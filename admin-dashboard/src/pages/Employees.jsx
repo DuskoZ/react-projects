d86fafa61bbb7bd8a5524afd3a58c79b28aada38
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-06-09 14:49:05 +0200

    added page markup

--- admin-dashboard/src/pages/Employees.jsx
+++ admin-dashboard/src/pages/Employees.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+    GridComponent,
+    ColumnsDirective,
+    ColumnDirective,
+    Search,
+    Page,
+    Inject,
+    Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
-    return <div>Employees</div>;
-};
+    //   const editing = { allowDeleting: true, allowEditing: true };
 
+    return (
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="Page" title="Employees" />
+            <GridComponent
+                dataSource={employeesData}
+                width="auto"
+                allowPaging
+                allowSorting
+                toolbar={["Search"]}
+                // pageSettings={{ pageCount: 5 }}
+                // editSettings={editing}
+            >
+                <ColumnsDirective>
+                    {employeesGrid.map((item, index) => (
+                        <ColumnDirective key={index} {...item} />
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[Page, Search, Toolbar]} />
+            </GridComponent>
+        </div>
+    );
+};
 export default Employees;
