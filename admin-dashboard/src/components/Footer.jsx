3a3955c1450169f14212dd7d291de71bf8a466e8
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-06-13 12:41:48 +0200

    added footer markup

--- admin-dashboard/src/components/Footer.jsx
+++ admin-dashboard/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div className="mt-12">
+            <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+                ©{new Date().getFullYear()} All rights reserved by 23design
+            </p>
+        </div>
+    );
 };
 
 export default Footer;
