8f37003117fc6c7303669117cfcfbb3fb7f40ac0
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-07-19 22:36:20 +0200

    minor changes

--- tabs/src/data/dummy.js
+++ tabs/src/data/dummy.js
@@ -7,7 +7,7 @@ export const data = [
         duties: [
             "Atque dolor, eaque assumenda quia recusandae esse cum hic ab nisi magnam, numquam eligendi ratione. Repellat tempore vero suscipit accusamus enim odit neque maiores.",
             "Dolorem asperiores maxime explicabo animi repudiandae perspiciatis corrupti dolorum totam doloremque corporis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quaerat ad blanditiis dolor officia, impedit molestiae velit, et odit.",
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe non, maxime deserunt nihil at quas odio fugit rerum, minima totam neque blanditiis ipsum, qui ex. Eligendi, ex aliquam ipsum voluptatem repellendus hic laborum nemo nam commodi veniam officia quia esse rerum deserunt cum alias beatae soluta veritatis pariatur molestiae autem unde vitae numquam.",
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe non, minima totam neque blanditiis ipsum, qui ex. Eligendi, ex quia esse rerum deserunt cum alias beatae soluta veritatis pariatur molestiae autem unde vitae numquam.",
         ],
         company: "Acme Corp.",
     },
@@ -17,10 +17,10 @@ export const data = [
         title: "Front-End Engineer",
         dates: "May 2017 - December 2019",
         duties: [
-            "Nam culpa vitae sint, distinctio tenetur error fugiat dicta alias nostrum deserunt esse necessitatibus reiciendis tempore autem nihil assumenda quod quae exercitationem. Commodi consectetur eius quod similique ipsum distinctio mollitia architecto nulla optio officiis expedita cum porro.",
-            "Dolorem natus cumque, voluptatum sapiente. Perferendis quidem voluptates ratione aperiam id! Nulla consequuntur, fuga aliquid illum vero odio voluptate dolor! Quae qui incidunt a enim sapiente unde nemo obcaecati quibusdam facere.",
+            "Nam culpa vitae sint, reiciendis tempore autem nihil assumenda quod quae exercitationem. Commodi consectetur eius quod similique ipsum distinctio mollitia architecto nulla optio officiis expedita cum porro.",
+            "Dolorem natus cumque, voluptatum sapiente. Fuga aliquid illum vero odio voluptate dolor! Quae qui incidunt a enim sapiente unde nemo obcaecati quibusdam facere.",
             "Similique, vero animi enim error doloribus cum? Corporis, sapiente magnam alias quas harum officiis suscipit aspernatur! Ad, molestias omnis quam rem earum iure. Nam animi tempora a.",
-            "Sint amet eius corrupti ipsam culpa repellat necessitatibus omnis cupiditate impedit! Corrupti ex exercitationem autem repudiandae veritatis recusandae ratione consectetur, dignissimos mollitia, repellendus modi temporibus, voluptatum eveniet aut ipsam fuga",
+            "Sint amet eius corrupti ex exercitationem autem repudiandae veritatis recusandae ratione consectetur, dignissimos mollitia, repellendus modi temporibus, voluptatum eveniet aut ipsam fuga",
         ],
         company: "WebTech",
     },
@@ -30,8 +30,8 @@ export const data = [
         title: "Web Designer",
         dates: "May 2013 - April 2017",
         duties: [
-            "Excepturi eius quae voluptates sit dolores cumque quo dolor reprehenderit nobis, debitis ab quis, magni, est repudiandae necessitatibus totam quaerat doloribus ratione rem beatae fugiat sint. Rerum autem iusto, obcaecati voluptatibus placeat tempore? Dolorem vitae cupiditate magnam placeat corrupti quos ut ipsum rem voluptatem.",
-            "A possimus placeat libero vitae repudiandae quis quisquam. Esse quia excepturi praesentium tempora! Exercitationem similique dolorum non saepe sint ad expedita adipisci magnam provident obcaecati assumenda nihil hic quis.",
+            "Est repudiandae necessitatibus totam quaerat doloribus ratione rem beatae fugiat sint. Rerum autem iusto, obcaecati voluptatibus placeat tempore? Dolorem vitae cupiditate magnam placeat corrupti quos ut ipsum rem voluptatem.",
+            "A possimus placeat libero vitae repudiandae quis quisquam. Exercitationem similique dolorum non saepe sint ad expedita adipisci magnam provident obcaecati assumenda nihil hic quis.",
             "Nulla aperiam voluptatem debitis doloribus assumenda doloremque error quaerat distinctio perspiciatis. Harum qui dolores, id at quae sit officiis similique.",
         ],
         company: "Designero",
