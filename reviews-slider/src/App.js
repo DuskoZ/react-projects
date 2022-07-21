859d98775bfc03218067a7c0eb9cdae1dd3e3658
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-07-21 21:09:34 +0200

    started slider markup

--- reviews-slider/src/App.js
+++ reviews-slider/src/App.js
@@ -1,22 +1,54 @@
-function App() {
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+
+import Title from "./components/Title";
+import Button from "./components/Button";
+import data from "./data/dummy";
+
+const App = () => {
+    const [people, setPeople] = useState(data);
+    const [index, setIndex] = useState(0);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <section className="section">
+            <Title title="reviews" />
+
+            <div className="section-center">
+                {people.map((person, personIndex) => {
+                    const { id, image, name, title, quote } = person;
+
+                    return (
+                        <article key={id}>
+                            {/* Person photo */}
+                            <img
+                                src={image}
+                                alt={name}
+                                className="person-img"
+                            />
+
+                            {/* Person name */}
+                            <h4 className="person-name">{name}</h4>
+
+                            {/* Person position */}
+                            <p className="person-role">{title}</p>
+
+                            {/* Person quote */}
+                            <p className="person-review">{quote}</p>
+
+                            <FaQuoteRight className="icon" />
+
+                            <Button icon={<FiChevronLeft />} className="prev" />
+                            <Button
+                                icon={<FiChevronRight />}
+                                className="next"
+                            />
+                        </article>
+                    );
+                })}
+            </div>
+        </section>
     );
-}
+};
 
 export default App;
