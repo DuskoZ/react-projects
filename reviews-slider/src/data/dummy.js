d99e7224da7d315a227d4be9970fbe39e14f2876
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-07-21 21:08:29 +0200

    minor fix

---
+++ reviews-slider/src/data/dummy.js
@@ -0,0 +1,32 @@
+const people = [
+    {
+        id: 1,
+        image: "./img/person1.jpg",
+        name: "maria petrovich",
+        title: "full stack developer",
+        quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum quidem, saepe maxime sapiente omnis molestiae!",
+    },
+    {
+        id: 2,
+        image: "./img/person2.jpg",
+        name: "matt cassey",
+        title: "carpenter",
+        quote: "Aliquam, nisi placeat quaerat dolorum vitae quae, voluptatum blanditiis tenetur, magni debitis eligendi! Suscipit error saepe dolores explicabo!",
+    },
+    {
+        id: 3,
+        image: "./img/person3.jpg",
+        name: "mark taylor",
+        title: "illustrator",
+        quote: "Eos a tempora molestias qui odit voluptate, numquam odio esse sequi, laudantium consequuntur rem porro itaque. Eveniet ipsa incidunt voluptatem ullam vel dicta quidem atque.",
+    },
+    {
+        id: 4,
+        image: "./img/person4.jpg",
+        name: "xenia andersdottir",
+        title: "racing driver",
+        quote: "Veniam, quisquam tempora molestiae repudiandae saepe, perferendis quibusdam deleniti itaque earum illo ratione voluptate aliquid reiciendis maiores vero aliquam dignissimos nostrum ex distinctio similique!",
+    },
+];
+
+export default people;
