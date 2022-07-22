a1a764d198621efaf4a9e15376dece8b651c4a38
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-07-22 23:26:24 +0200

    added passed class as props

--- reviews-slider/src/components/Button.jsx
+++ reviews-slider/src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = (props) => {
-    return <button>{props.icon}</button>;
+    return <button className={props.cssClass}>{props.icon}</button>;
 };
 
 export default Button;
